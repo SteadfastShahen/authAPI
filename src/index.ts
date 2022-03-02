d5150c3f53bfd519029fba7d91d121b1bcf2ab46
Shahen <shahen@steadfast.tech>
2022-03-02 14:52:02 +0400

    dist deleted

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
+import '../env';
 import express, { Express, Request, Response } from 'express';
 import { connect } from 'mongoose';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-//mongoose
 connect('mongodb://localhost:27017/users-db', () => {
     console.log('Successfully connected to db')
 });
-
-// connect('mongodb+srv://testuser:KLy0KYHe1YlbO4CR@maincluster.7q6fv.mongodb.net/users-db?retryWrites=true&w=majority', ()=>{console.log('Successfully connected to db')});
 
 const PORT =  process.env.PORT || 3000;
 
